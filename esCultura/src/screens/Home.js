d017763e4638d02bfbb41426d16f8a47c87d94b8
QuimTorra <q.torra@gmail.com>
2023-03-21 13:34:31 +0100

    👨‍✈️ Basic Bottom Navigation Bar

---
+++ esCultura/src/screens/Home.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function Home(props) {
+    return (
+        <Text>HOME : {props.title}</Text>
+    );
+}
---
+++ esCultura/src/screens/Search.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function Search(props) {
+    return (
+        <Text>SEARCH</Text>
+    );
+}
