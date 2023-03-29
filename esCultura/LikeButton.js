bd91aeb6802bf70626a955b1a1df719676f60129
tania.alonso <tania.alonso@estudiantat.upc.edu>
2023-03-29 11:40:47 +0200

    tasca 72 i 72 acabades

--- esCultura/LikeButton.js
+++ esCultura/LikeButton.js
@@ -1,41 +1,62 @@
-import React, { useState } from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const LikeButton = ({ itemId} ) => {
+const LikeButton =  ( ) => {
     const [liked, setLiked] = useState(false);
-  
-    // aixo serveix per canviar el disseny. 
-    const handlePress = async () => {      
-      if (liked) {
-          /* fetch('https://tu-api-backend.com/tu-endpoint', {
-          method: 'PUT', // método PUT
+    const [likes, setLikes] = useState(0);
+    const likeValue = liked ? -1 : 1;
+    
+   useEffect(() => {
+    const fetchLikes = async () => {
+      try {
+        const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments?id==20230315095' );
+        //.then ( response => response.json() );
+        if (!response.ok) {
+          throw new Error('Error al obtener el número de likes');
+        }    
+        
+        const data = await response.json();
+        if (!data.likes) data.likes = 0;
+        setLikes(data.likes);
+        
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  fetchLikes();
+  }, []);
+
+  const handlePress = async () => {     
+          try {
+          const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments?id==20230315095', {
+          method: 'PUT', 
           headers: {
-            'Content-Type': 'application/json', // tipo de contenido que estás enviando
+            'Content-Type': 'application/json', 
           },
           body: JSON.stringify({ 
-            // datos que deseas actualizar en formato JSON
-            key1: value1,
-            key2: value2,
-            // ...
+            likeValue: likeValue,
           }),
-        })
-        //.then((response) => response.json())
-        .catch((error) => {
-          console.error(error);
-        }); */
-
-      }  
-      else {
-      }
-      //setLiked(response.data.liked);
+        });
+        if (!response.ok) {
+          throw new Error('Error al enviar solicitud');
+        
+        }  
+      setLikes((prevLikes) => prevLikes + likeValue);
       setLiked(!liked);
-    };
+      } catch (error) {
+        console.error(error);
+      }
+  }
+    
     return (
       <TouchableOpacity onPress={handlePress}>
         <Icon name={liked ? 'heart' : 'heart-o'} size={24} color={liked ? 'red' : 'black'} />
+        <Text> {likes} Likes </Text>
       </TouchableOpacity>
+
     );
   };
 
-export default LikeButton;
+
+  export default LikeButton;
