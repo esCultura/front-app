73a54ca720eff46301e4146fb46d954e7ab687ce
tania.alonso <tania.alonso@estudiantat.upc.edu>
2023-03-30 17:33:39 +0200

    tasca 72 i 73 ja funcina del tot

--- esCultura/LikeButton.js
+++ esCultura/LikeButton.js
@@ -6,33 +6,28 @@ const LikeButton =  ( ) => {
     const [liked, setLiked] = useState(0);
     const [likes, setLikes] = useState(0);
     const likeValue = liked ? -1 : 1;
+    const perfil = "primerUsuari";
+    const esdeveniment = 20230315095;
     
    useEffect(() => {
     const fetchLikes = async () => {
       try {
-        const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments?esdeveniment=20230315095' );
+        const response = await fetch( `http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments/?esdeveniment=${esdeveniment}` );
         if (!response.ok) {
           throw new Error('Error al obtener el número de likes');
         }    
         const data = await response.json();
         setLikes(data.length);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  fetchLikes();
-  }, []);
+        if (data.length ==  0) setId(0);
 
-  useEffect(() => {
-    const fetchLikes = async () => {
-      try {
-        const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments?perfil=primerUsuari' );
-        if (!response.ok) {
-          throw new Error('Error al obtener el likes');
+        const response2 = await fetch( `http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments/?perfil=${perfil}&esdeveniment=${esdeveniment}` );
+        if (!response2.ok) {
+          throw new Error('Error al obtener el like');
         }    
-        const data = await response.json();
-        if (data.length === 0) setLiked(false);
-        else setLiked(true);
+        const data2 = await response2.json();
+        if (data2.length === 0)  setLiked(false);
+        else    setLiked(true);
+
     } catch (error) {
       console.error(error);
     }
@@ -40,27 +35,55 @@ const LikeButton =  ( ) => {
   fetchLikes();
   }, []);
 
-  const handlePress = async () => {     
-          try {
-          const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments?esdeveniment=20230315095', {
-          method: 'PUT', 
-          headers: {
-            'Content-Type': 'application/json', 
-          },
-          body: JSON.stringify({ 
-            perfil: "primerUsuari",
-            esdeveniment: "20230315095",
-          }),
-        });
-        if (!response.ok) {
-          throw new Error('Error al enviar solicitud');
-        }  
-      setLikes((prevLikes) => prevLikes + likeValue);
-      setLiked(!liked);
-      } catch (error) {
-        console.error(error);
+  const handleLike = async () => {
+    try {
+      const response = await fetch('http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          perfil: perfil,
+          esdeveniment: esdeveniment,
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Error al enviar solicitud');
       }
-  }
+  
+      setLikes((prevLikes) => prevLikes + likeValue);
+      setLiked(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const handleUnlike = async () => {
+    try {
+      const response = await fetch(`http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments/?perfil=${perfil}&esdeveniment=${esdeveniment}`, {
+        method: 'DELETE',
+      });
+  
+      if (!response.ok) {
+        throw new Error('Error al enviar solicitud1');
+      }
+  
+      setLikes((prevLikes) => prevLikes + likeValue);
+      setLiked(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const handlePress = () => {
+    if (liked) {
+      handleUnlike();
+    } else {
+      handleLike();
+    }
+  };
+  
     
     return (
       <TouchableOpacity onPress={handlePress}>
