22f7f466a2b150c12db7ef222c7500833cb4b451
tania.alonso <tania.alonso@estudiantat.upc.edu>
2023-03-30 12:33:45 +0200

    tasca 72 acabada

--- esCultura/src/components/LikeButton.js
+++ esCultura/src/components/LikeButton.js
@@ -6,6 +6,7 @@ const LikeButton =  ( ) => {
     const [liked, setLiked] = useState(0);
     const [likes, setLikes] = useState(0);
     const likeValue = liked ? -1 : 1;
+    const [id, setId] = useState(0); 
     
    useEffect(() => {
     const fetchLikes = async () => {
@@ -16,6 +17,7 @@ const LikeButton =  ( ) => {
         }    
         const data = await response.json();
         setLikes(data.length);
+        if (data.length ==  0) setId(0);
     } catch (error) {
       console.error(error);
     }
@@ -26,13 +28,19 @@ const LikeButton =  ( ) => {
   useEffect(() => {
     const fetchLikes = async () => {
       try {
-        const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments?perfil=primerUsuari' );
+        const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments?perfil=primerUsuari&esdeveniment=20230315095' );
         if (!response.ok) {
-          throw new Error('Error al obtener el likes');
+          throw new Error('Error al obtener el like');
         }    
         const data = await response.json();
-        if (data.length === 0) setLiked(false);
-        else setLiked(true);
+        if (data.length === 0)  {
+          setLiked(false);
+          setId(0);
+        }
+        else  {
+          setLiked(true);
+          setId(data[0].id);
+        }
     } catch (error) {
       console.error(error);
     }
@@ -41,9 +49,32 @@ const LikeButton =  ( ) => {
   }, []);
 
   const handlePress = async () => {     
-          try {
-          const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments?esdeveniment=20230315095', {
-          method: 'PUT', 
+    if (liked) {
+        try {
+          const response = await fetch( `http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments/?${id}`, {
+          method: 'DELETE', 
+          /*headers: {
+            'Content-Type': 'application/json', 
+          },
+          body: JSON.stringify({ 
+            id: id,
+            //perfil: "primerUsuari",
+            //esdeveniment: "20230315095",
+          }),*/
+        });
+        if (!response.ok) {
+          throw new Error('Error al enviar solicitud1');
+        } 
+      setLikes((prevLikes) => prevLikes + likeValue);
+      setLiked(!liked);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+      else {
+        try {
+          const response = await fetch( 'http://deploy-env.eba-6a6b2amf.us-west-2.elasticbeanstalk.com/interessos/esdeveniments', {
+          method: 'POST', 
           headers: {
             'Content-Type': 'application/json', 
           },
@@ -54,18 +85,20 @@ const LikeButton =  ( ) => {
         });
         if (!response.ok) {
           throw new Error('Error al enviar solicitud');
-        }  
+        } 
       setLikes((prevLikes) => prevLikes + likeValue);
       setLiked(!liked);
       } catch (error) {
         console.error(error);
-      }
+      }}
+
   }
     
     return (
       <TouchableOpacity onPress={handlePress}>
         <Icon name={liked ? 'heart' : 'heart-o'} size={24} color={liked ? 'red' : 'black'} />
         <Text> {likes} Likes </Text>
+        <Text> {id} id </Text>
       </TouchableOpacity>
 
     );
