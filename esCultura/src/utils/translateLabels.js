603c154e88e887e3d5bf604c4a4cb044d4c24c6d
angelPrat <angel.prat@estudiantat.upc.edu>
2023-04-18 19:08:59 +0200

    Task #193 traduccio de les etiquetes

--- esCultura/src/utils/translateLabels.js
+++ esCultura/src/utils/translateLabels.js
@@ -1,14 +1,13 @@
-export default {
-    cat: {
-        start: "començar"
-    },
-    cas: {
-        start: "empezar"
-    },
-    eng: {
-        start: "start"
-    }
+const en = {
+    search: "Search"
 }
 
+const es = {
+    search: "Buscar"
+}
 
-{{TransformStreamDefaultController[cat].start}}
+const cat = {
+    search: "Buscar"
+}
+
+export {en, cat, es};
