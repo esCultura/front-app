4ad4078d7a5da138efdd1fcc96b71325976987b8
angelPrat <angel.prat@estudiantat.upc.edu>
2023-05-07 16:36:50 +0200

    util function test complete

--- esCultura/Tests/utilFunctions.test.js
+++ esCultura/Tests/utilFunctions.test.js
@@ -9,5 +9,28 @@ test("set token test", () => {
     expect(getToken()).toBe("tokenTest");
 })
 
+//representa que es lo que retorna el fetch
+global.fetch = jest.fn(() => Promise.resolve({
+    json: () => Promise.resolve({
+        codi:  20191011001
+    })
+}))
 
+test("simple fetch get", () => {
+    setToken("4399aea952484e30ad0208cd72bf64a083c9b8c4");
+    simpleFetch("esdeveniments/?limit=1", "GET", "").then((data) => {
+        expect(data.codi).toBe(20191011001);
+    });
+})
+
+
+test("simple fetch post", () => {
+    setToken("4399aea952484e30ad0208cd72bf64a083c9b8c4");
+    simpleFetch("seguiments/", "POST", {
+        "seguidor": 1,
+        "seguit": 2
+    }).then((data) => {
+        expect(data.codi).toBe(20191011001);
+    });
+})
 
