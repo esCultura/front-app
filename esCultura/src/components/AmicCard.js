b0d8251804fd336b59f0077727c8e156cc732fbb
tania.alonso <tania.alonso@estudiantat.upc.edu>
2023-05-25 17:42:27 +0200

    Revert "Revert "Merge branch 'develop' into Pdf""
    This reverts commit 4b21360c0402f9301c6c1a26a4f318739f0c66d4.

---
+++ esCultura/src/components/AmicCard.js
@@ -0,0 +1,137 @@
+import {
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Esdeveniment from "./Esdeveniment";
+import { simpleFetch } from "../utils/utilFunctions";
+import { useEffect, useState } from "react";
+import InfoCompleta from "./InfoCompleta";
+import XCircleFill from "react-native-bootstrap-icons/icons/x-circle-fill";
+import PerfilSimple from "./PerfilSimple";
+
+export default function AmicCard(props) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalPerfil, setModalPerfil] = useState(false);
+
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.userContainer}
+          onPress={() => {
+            setModalPerfil(true);
+          }}
+        >
+          <Image
+            style={styles.profileImage}
+            source={{
+              uri: props.info.p_imatge
+                ? props.info.p_imatge
+                : "https://static.wikia.nocookie.net/amogus/images/4/4d/OBUNGUS.jpg/revision/latest?cb=20220210101127",
+            }}
+          />
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+            {props.info.p_nom}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            padding: 15,
+          }}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: "bold",
+              maxWidth: 150,
+              textAlign: "left",
+            }}
+          >
+            {props.info.e_titol}
+          </Text>
+          {Number.parseInt(props.info.e_data.slice(0, 4)) < 2050 ? (
+            <>
+              <Text style={{ fontWeight: "300", textAlign: "left" }}>
+                🗓️ {props.info.e_data.slice(0, 10)}
+              </Text>
+              <Text style={{ fontWeight: "300", textAlign: "left" }}>
+                📌 {props.info.e_loc}
+              </Text>
+            </>
+          ) : (
+            <Text style={{ fontWeight: "300", textAlign: "left" }}>
+              🌐 Online
+            </Text>
+          )}
+        </TouchableOpacity>
+      </View>
+      <InfoCompleta
+        visible={modalVisible}
+        back={() => {
+          setModalVisible(false);
+        }}
+        perfil={props.info.e_perfil}
+        type={props.info.e_type}
+        title={props.info.e_titol}
+        preu={props.info.e_preu}
+        complet={props.info.e_desc}
+        dateIni={props.info.e_data}
+        dateFi={props.info.e_dataFi}
+        location={props.info.e_loc}
+        source={props.info.e_source}
+        codi={props.info.e_codi}
+      />
+      <Modal visible={modalPerfil}>
+        <TouchableOpacity
+          onPress={() => {
+            setModalPerfil(false);
+          }}
+          style={styles.backperfil}
+        >
+          <XCircleFill color="red" width={145} height={145} />
+        </TouchableOpacity>
+        <PerfilSimple id={props.info.p_user}> </PerfilSimple>
+      </Modal>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    borderRadius: 13,
+    backgroundColor: "#E9E9E9",
+    paddingVertical: 10,
+    height: 135,
+  },
+  userContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "35%",
+    shadowColor: "rgba(255, 0, 0, 0.25)",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowRadius: 4,
+    shadowOpacity: 1,
+    borderRightColor: "#44941E",
+    borderRightWidth: 1,
+  },
+  profileImage: {
+    width: 75,
+    height: 75,
+    borderColor: "#3347FF",
+    borderWidth: 5,
+    borderRadius: 40,
+  },
+});
